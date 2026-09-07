df06f023d7d2191fe9ab50ae217ba8554fffee79
Marzm <marzm@qq.com>
2026-09-07 16:23:50 +0800

    fix: 适配新版的请求地址

--- request/index.js
+++ request/index.js
@@ -2,21 +2,21 @@ import request from "@/utils/request"
 
 export function getDogMessage() {
   return request({
-    url: "/api/message/dog",
+    url: "/app/message/audit/dog",
     method: "get"
   })
 }
 
 export function getCaptcha() {
   return request({
-    url: "/api/base/captcha?height=45&width=150&color=%232c3142",
+    url: "/app/base/comm/captcha?height=45&width=150&color=%232c3142",
     method: "get"
   })
 }
 
 export function submitMessageAudit(data) {
   return request({
-    url: "/api/message/audit",
+    url: "/app/message/audit",
     method: "post",
     data
   })
--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [uni()],
   server: {
     proxy: {
-      "/api": {
+      "/app": {
         target: BASEURL,
         changeOrigin: true
       }
